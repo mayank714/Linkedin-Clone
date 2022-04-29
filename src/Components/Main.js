9e885f13b15ce12fd7aeb18c2e6f6021b8c7b7c2
Mayank Verma <vermamayank714@gmail.com>
2022-04-29 18:32:47 +0530

    Building Main content

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Main = (props) => {
   return <Container><Sharebox>
     Share
-  </Sharebox>
+  
   <div>
     <img src="/images/user.svg"></img>
     <button> Start a Post</button>
@@ -21,7 +21,12 @@ const Main = (props) => {
     <img src="/images/event-icon.svg"></img>
     <span>Event</span>
     </button>
+    <button>
+    <img src="/images/articke-icon.svg"></img>
+    <span>Write an article</span>
+    </button>
   </div>
+  </Sharebox>
   </Container>;
 };
 
@@ -44,6 +49,59 @@ flex-direction:column;
 color:#958b7b;
 margin:0 0 8px;
 background:white;
+div{
+  button{
+    outline:none ;
+    color:rgba(0,0,0,0.6);
+    font-size:14px;
+    line-height:1.5;
+    min-height:48px;
+    background:transparent ;
+    border:none;
+    display:flex;
+    align-items:center;
+    font-weight:600;
+  }
+  &:first-child{
+    display:flex;
+    align-items:center;
+    padding:8px 16px 0px 16px;
+    img{
+      /* background-color:white ; */
+      width:48px;
+      border-radius:50%;
+      margin-right:8px;
+
+    }
+    button{
+      margin:4px 0;
+      flex-grow:1;
+      border-radius:35px;
+      padding-left:16px;
+      border:1px solid rgba(0,0,0,0.15);
+      background-color:white;
+      text-align:left;
+
+    }
+    
+  }
+  &:nth-child(2){
+      display:flex;
+      flex-wrap:wrap;
+      justify-content:space-around ;
+      padding-bottom:4px;
+      button{
+        img{
+          margin:0 4px 0 -2px ;
+        }
+        span{
+          color:#70b5f9;
+        }
+      }
+
+    }
+
+}
 `;
 
 export default Main;
